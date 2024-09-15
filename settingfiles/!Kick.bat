e9a89c55b0c1a95d3e3e2fe75ae07859cad5be6a
watuwa3 <watuwa3@gmail>
2024-09-15 21:56:55 +0900

    バッチファイルのログが見やすくなるよう修正

--- settingfiles/!Kick.bat
+++ settingfiles/!Kick.bat
@@ -1,6 +1,12 @@
 @echo off
+set logDate=%date:~0,4%-%date:~5,2%-%date:~8,2%
 set yyyyMMdd=%date:/=%
 echo.
-echo ＹＧＷ対象ファイルチェック[%yyyyMMdd%]
+echo ＹＧＷ対象ファイルチェック[%logDate%]
 echo.
-RefreshYGW.exe >> .\%yyyyMMdd%_RefreshYGW.log
+if not exist "%yyyyMMdd%_RefreshYGW.log" (
+	echo ＹＧＷ対象ファイルチェック > %yyyyMMdd%_RefreshYGW.log
+	echo. >> %yyyyMMdd%_RefreshYGW.log
+)
+echo 処理日時：[%logDate%%time:~0,8%] >> %yyyyMMdd%_RefreshYGW.log
+RefreshYGW.exe >> %yyyyMMdd%_RefreshYGW.log
